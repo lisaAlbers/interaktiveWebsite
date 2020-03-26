e229c09dd6de6280f4cf4ba1b68b03961055b447
Lisa Albers <lisa.albers97@gmail.com>
2020-03-26 11:51:31 +0100

    Adding a little mouse effect

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ counters.forEach(counter => {
 
 	updateCount();
 });
+document.onmousemove = animateCircles;
+var colors = ['#ccc','#6cf','#eba13a'];
+function animateCircles (event){
+    var circle = document.createElement("div");
+    circle.setAttribute("class","circle");
+    document.body.appendChild(circle);
+
+    circle.style.left = event.clientX + 'px';
+    circle.style.top = event.clientY + 'px';
+
+    circle.style.transition = "all 0.5s linear 0s";
+
+    circle.style.left = circle.offsetLeft - 20 + 'px';
+    circle.style.top = circle.offsetTop - 20 + 'px';
+
+    circle.style.width = "50px";
+    circle.style.height = "50px";
+    circle.style.borderWidth = "5px";
+    circle.style.opacity = 0;
+
+}
