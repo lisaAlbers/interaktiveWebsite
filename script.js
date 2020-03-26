2707e52996c06c35c71318760185ff4f1b82fb67
Lisa Albers <lisa.albers97@gmail.com>
2020-03-26 11:01:22 +0100

    Adding a Counter to the code
    Hinzufügen eines Counters für Subscribers, Followers, Likes und Conections

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const logo = document.querySelector("#logo");
-const menu = document.querySelector("#menu");
-const banner  = document.querySelector("#banner ");
-const container  = document.querySelector("#container");
+const counters = document.querySelectorAll('.counter');
+const speed = 200; // The lower the slower
 
-const tl = new TimelineMax();
-tl.fromTo(banner,1,{height:"0%"},{height:"80%",ease:Power2.easeInOut})
-.fromTo(banner,1.2, 
-    {width:"100%"}, 
-    {width:"80%",ease: Power2.easeInOut}
-    )
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+		// console.log(inc);
+		// console.log(count);
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = count + inc;
+			// Call function every ms
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
