4f4017fd751f4428ebe416cb0a38a473792f222f
Lisa Albers <lisa.albers97@gmail.com>
2020-03-27 13:29:01 +0100

    Adding a Slideshow
    Hinzufügen von einer Bilderslideshow

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+document.addEventListener('keydown', function(event) {
+    var ekey = event.keyCode
+    if( 37 === ekey ) {
+      pauseSlideshow();
+    previousSlide();
+    } 
+  if ( 39 === ekey ) {
+       // right arrow
+    pauseSlideshow();
+    nextSlide();
+    }});
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide() {
+    goToSlide(currentSlide+1);
+}
+
+function previousSlide() {
+    goToSlide(currentSlide-1);
+}
+
+function goToSlide(n) {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (n+slides.length)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+
+var playing = true;
+var pauseButton = document.getElementById('pause');
+
+function pauseSlideshow() {
+    pauseButton.innerHTML = '&#9658';
+    playing = false;
+    clearInterval(slideInterval);
+}
+
+function playSlideshow() {
+    pauseButton.innerHTML = '||';
+    playing = true;
+    slideInterval = setInterval(nextSlide,2000);
+}
+
+pauseButton.onclick = function() {
+    if(playing) {
+    pauseSlideshow();
+  } else {
+    playSlideshow();
+  }
+};
+
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+
+next.onclick = function() {
+    pauseSlideshow();
+    nextSlide();
+};
+previous.onclick = function() {
+    pauseSlideshow();
+    previousSlide();
+};
 const counters = document.querySelectorAll('.counter');
 const speed = 200; // The lower the slower
 
@@ -39,6 +103,9 @@ function animateCircles (event){
 
     circle.style.left = circle.offsetLeft - 20 + 'px';
     circle.style.top = circle.offsetTop - 20 + 'px';
+
+   var color = colors[ Math.floor(Math.random()*colors.length)];
+   circle.style.borderColor = color;
 
     circle.style.width = "50px";
     circle.style.height = "50px";
